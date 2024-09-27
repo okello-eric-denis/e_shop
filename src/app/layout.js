0deeab598a2083f474856a83133f8dd41606fc76
okello-eric-denis <okelloericdenis@gmail.com>
2024-09-27 18:17:49 +0300

    front end finished

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import MobNavbar from "@/components/MobNavbar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({ children }) {
         <Navbar/>
         <MobNavbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
