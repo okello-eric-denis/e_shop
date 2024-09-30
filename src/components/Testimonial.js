b060712d2610bf1cbba33b3d5dbdef81810c1d5f
okello-eric-denis <okelloericdenis@gmail.com>
2024-09-30 19:31:41 +0300

    changed name

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -16,7 +16,7 @@ const Testimonial = () => {
                 height={80}
                 alt="profile"
               />
-              <h2 className="text-gray-500 font-black text-[20px]">Elsa Doe</h2>
+              <h2 className="text-gray-500 font-black text-[20px]">Elzie</h2>
               <p>CEO & Founder Invision</p>
               <Image
                 className="inline-block py-2"
